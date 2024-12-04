670c28868bbcb8d59fb3087268f1120495600bf2
Radoslaw Wydro <r.wydro@zoovu.com>
2024-12-04 20:45:05 +0100

    init

---
+++ FamilyAppFe/metro.config.js
@@ -0,0 +1,25 @@
+const { getDefaultConfig } = require('expo/metro-config');
+module.exports = {
+    ...getDefaultConfig(__dirname),
+    transformer: {
+        ...getDefaultConfig(__dirname).transformer,
+        sourceMaps: true,
+        experimentalImportSupport: false,
+        inlineRequires: true,
+    },
+    resolver: {
+        ...getDefaultConfig(__dirname).resolver,
+        sourceMaps: true,
+        minifierConfig: {
+            mangle: false,
+            keep_fnames: true,
+        },
+    },
+    env: {
+        production: {
+            plugins: ['react-native-paper/babel'],
+        },
+    },
+    presets: ['babel-preset-expo'],
+    watchFolders: [__dirname + '/src'],
+};
